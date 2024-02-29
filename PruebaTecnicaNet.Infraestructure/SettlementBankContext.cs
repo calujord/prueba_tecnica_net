ae4b002b8c27341fc45c42cbb851354ca1f74f55
David Lozano Rivada <dlrivada@hotmail.com>
2024-02-29 22:35:33 +0100

    The most significant changes involve the addition of a new class `SettlementBankContext` in the `PruebaTecnicaNet.Infraestructure` namespace. This class extends the `DbContext` class and implements the `IUnitOfWork` interface. It contains a `DbSet` property `SettlementBanks` of type `SettlementBank` and two private fields: `_mediator` and `_currentTransaction`. The class also includes two constructors, several methods for transaction management, and the enabling of nullable reference types.
    Here are the changes in detail:
    
    1. A new class `SettlementBankContext` has been added to the `PruebaTecnicaNet.Infraestructure` namespace. This class extends the `DbContext` class and implements the `IUnitOfWork` interface.
    
    2. The `SettlementBankContext` class has a `DbSet` property `SettlementBanks` of type `SettlementBank`.
    
    3. The class has two private fields: `_mediator` of type `IMediator` and `_currentTransaction` of type `IDbContextTransaction`.
    
    4. Two constructors have been added to the class. The first constructor takes `DbContextOptions<SettlementBankContext>` as a parameter. The second constructor takes `DbContextOptions<SettlementBankContext>` and `IMediator` as parameters. If the `IMediator` parameter is null, an `ArgumentNullException` is thrown.
    
    5. The class has a method `GetCurrentTransaction` that returns the current transaction.
    
    6. The class has a property `HasActiveTransaction` that checks if a current transaction exists.
    
    7. The `OnModelCreating` method has been overridden to set the default schema to "bank" and apply configurations from the assembly of `SettlementBankContext`.
    
    8. The class has a method `SaveEntitiesAsync` that dispatches domain events and saves changes to the database.
    
    9. The class has a method `BeginTransactionAsync` that begins a new transaction if there isn't a current transaction.
    
    10. The class has a method `CommitTransactionAsync` that commits the current transaction and saves changes to the database. If the transaction parameter is null or not the current transaction, an exception is thrown.
    
    11. The class has a method `RollbackTransaction` that rolls back the current transaction.
    
    12. Nullable reference types have been enabled for the class with `#nullable enable`.

---
+++ PruebaTecnicaNet.Infraestructure/SettlementBankContext.cs
@@ -0,0 +1,100 @@
+﻿namespace PruebaTecnicaNet.Infraestructure;
+
+public class SettlementBankContext : DbContext, IUnitOfWork
+{
+    public DbSet<SettlementBank> SettlementBanks { get; set; }
+
+    private readonly IMediator _mediator;
+    private IDbContextTransaction _currentTransaction;
+
+    public SettlementBankContext(DbContextOptions<SettlementBankContext> options) : base(options)
+    {
+    }
+
+    public SettlementBankContext(DbContextOptions<SettlementBankContext> options, IMediator mediator) : base(options)
+    {
+        _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+
+        System.Diagnostics.Debug.WriteLine("SettlementBankContext::ctor ->" + this.GetHashCode());
+    }
+
+    public IDbContextTransaction GetCurrentTransaction() => _currentTransaction;
+
+    public bool HasActiveTransaction => _currentTransaction != null;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("bank");
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SettlementBankContext).Assembly);
+        // TODO: logs here
+    }
+
+    public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default)
+    {
+        // Dispatch Domain Events collection. 
+        // Choices:
+        // A) Right BEFORE committing data (EF SaveChanges) into the DB will make a single transaction including  
+        // side effects from the domain event handlers which are using the same DbContext with "InstancePerLifetimeScope" or "scoped" lifetime
+        // B) Right AFTER committing data (EF SaveChanges) into the DB will make multiple transactions. 
+        // You will need to handle eventual consistency and compensatory actions in case of failures in any of the Handlers. 
+        await _mediator.DispatchDomainEventsAsync(this);
+
+        // After executing this line all the changes (from the Command Handler and Domain Event Handlers) 
+        // performed through the DbContext will be committed
+        _ = await base.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+
+    public async Task<IDbContextTransaction> BeginTransactionAsync()
+    {
+        if (_currentTransaction != null) return null;
+
+        _currentTransaction = await Database.BeginTransactionAsync(IsolationLevel.ReadCommitted);
+
+        return _currentTransaction;
+    }
+
+    public async Task CommitTransactionAsync(IDbContextTransaction transaction)
+    {
+        if (transaction == null) throw new ArgumentNullException(nameof(transaction));
+        if (transaction != _currentTransaction) throw new InvalidOperationException($"Transaction {transaction.TransactionId} is not current");
+
+        try
+        {
+            await SaveChangesAsync();
+            transaction.Commit();
+        }
+        catch
+        {
+            RollbackTransaction();
+            throw;
+        }
+        finally
+        {
+            if (_currentTransaction != null)
+            {
+                _currentTransaction.Dispose();
+                _currentTransaction = null;
+            }
+        }
+    }
+
+    public void RollbackTransaction()
+    {
+        try
+        {
+            _currentTransaction?.Rollback();
+        }
+        finally
+        {
+            if (_currentTransaction != null)
+            {
+                _currentTransaction.Dispose();
+                _currentTransaction = null;
+            }
+        }
+    }
+}
+
+#nullable enable
