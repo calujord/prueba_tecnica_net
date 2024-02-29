5c6658071d5ef2c3b5e4a99cce81b285bc304b3b
David Lozano Rivada <dlrivada@hotmail.com>
2024-02-29 22:37:29 +0100

    The most significant changes involve the creation of a new interface `IRequestManager` and its implementation `RequestManager` in the `PruebaTecnicaNet.Infraestructure.Idempotency` namespace. The `IRequestManager` interface defines two methods, `ExistAsync` and `CreateRequestForCommandAsync`, which are implemented in the `RequestManager` class. The `RequestManager` class also includes a private field `_context` of type `SettlementBankContext` and a constructor that initializes this field.
    List of changes:
    
    1. A new interface `IRequestManager` was created in the `PruebaTecnicaNet.Infraestructure.Idempotency` namespace. This interface defines two methods: `ExistAsync` and `CreateRequestForCommandAsync`. The `ExistAsync` method takes a `Guid` as an argument and returns a `Task<bool>`, while the `CreateRequestForCommandAsync` method takes a `Guid` and a generic type `T` as arguments and returns a `Task`.
    
    2. The `RequestManager` class was created in the same namespace as the `IRequestManager` interface. This class implements the `IRequestManager` interface. It has a private field `_context` of type `SettlementBankContext` and a constructor that initializes this field.
    
    3. The `CreateRequestForCommandAsync` method in the `RequestManager` class checks if a request with the given `id` already exists by calling the `ExistAsync` method. If it does, it throws a `SettlementBankException`. Otherwise, it creates a new `ClientRequest` with the `id`, the name of the type `T`, and the current UTC time, adds it to the `_context`, and saves the changes.
    
    4. The `ExistAsync` method in the `RequestManager` class finds a `ClientRequest` with the given `id` in the `_context` and returns `true` if it exists and `false` otherwise.
    
    References to the code changes can be found in the `PruebaTecnicaNet.Infraestructure.Idempotency` namespace.

---
+++ PruebaTecnicaNet.Infraestructure/Idempotency/IRequestManager.cs
@@ -0,0 +1,8 @@
+﻿namespace PruebaTecnicaNet.Infraestructure.Idempotency;
+
+public interface IRequestManager
+{
+    Task<bool> ExistAsync(Guid id);
+
+    Task CreateRequestForCommandAsync<T>(Guid id);
+}
