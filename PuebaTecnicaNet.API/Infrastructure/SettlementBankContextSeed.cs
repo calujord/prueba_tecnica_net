30f25b58d64a6167b5ce97d60ad11fb7f8456279
David Lozano Rivada <dlrivada@hotmail.com>
2024-03-02 23:10:08 +0100

    The most significant changes include the addition of a new namespace, the creation of a new public class, and the addition of an asynchronous method within this class.
    1. A new namespace `PruebaTecnicaNet.API.Infrastructure` has been added. This change suggests that new functionalities related to infrastructure are being introduced in the API.
    
    2. Several new `using` directives have been added, which are `PruebaTecnicaNet.API`, `PruebaTecnicaNet.Domain.SettlementBankAggregate`, `PruebaTecnicaNet.Infraestructure`, and `System.Threading.Tasks`. These directives indicate that the code in the file is using types defined in other namespaces.
    
    3. A new public class `SettlementBankContextSeed` has been created which implements the `IDbSeeder<SettlementBankContext>` interface. This class is likely to be used for seeding data into the `SettlementBankContext` database.
    
    4. Inside the `SettlementBankContextSeed` class, an asynchronous method `SeedAsync` has been added which takes an instance of `SettlementBankContext` as a parameter. This method is responsible for seeding data into the database.
    
    5. Inside the `SeedAsync` method, the `SaveChangesAsync` method is called on the `context` object. This method is used to save all changes made in the `SettlementBankContext` to the underlying database. This indicates that the `SeedAsync` method is not only responsible for creating the data but also for saving it to the database.

---
+++ PuebaTecnicaNet.API/Infrastructure/SettlementBankContextSeed.cs
@@ -0,0 +1,14 @@
+﻿namespace PruebaTecnicaNet.API.Infrastructure;
+
+using PruebaTecnicaNet.API;
+using PruebaTecnicaNet.Domain.SettlementBankAggregate;
+using PruebaTecnicaNet.Infraestructure;
+using System.Threading.Tasks;
+
+public class SettlementBankContextSeed : IDbSeeder<SettlementBankContext>
+{
+    public async Task SeedAsync(SettlementBankContext context)
+    {
+        await context.SaveChangesAsync();
+    }
+}
