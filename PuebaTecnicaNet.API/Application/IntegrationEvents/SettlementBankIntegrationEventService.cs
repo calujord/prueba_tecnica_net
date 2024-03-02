1c7bc8b224f2c3c9e803f00e5a94c38d44b36b85
David Lozano Rivada <dlrivada@hotmail.com>
2024-03-02 23:12:06 +0100

    The most significant changes involve the addition of new files that handle integration events related to the updating of settlement banks. These changes include the creation of a new event handler, an integration event record, an interface for the integration event service, and the implementation of this interface.
    1. A new file, `SettlementBankUpdatedSucceedIntegrationEventHandler.cs`, has been added. This file contains a class that implements the `IIntegrationEventHandler` interface, logging the handling of an integration event and sending a `CreateSettlementBankCommand` command.
    
    2. The `SettlementBankUpdatedSucceedIntegrationEvent.cs` file has been added. This file contains a record that inherits from the `IntegrationEvent` class and includes three properties: `Bic`, `Name`, and `Country`.
    
    3. A new interface, `ISettlementBankIntegrationEventService`, has been added in the `ISettlementBankIntegrationEventService.cs` file. This interface declares two methods for publishing and saving events.
    
    4. The `SettlementBankIntegrationEventService.cs` file has been added. This file contains a class that implements the `ISettlementBankIntegrationEventService` interface. The class includes methods for retrieving and publishing pending event logs, handling errors during publishing, and logging and saving events.

---
+++ PuebaTecnicaNet.API/Application/IntegrationEvents/SettlementBankIntegrationEventService.cs
@@ -0,0 +1,47 @@
+﻿using PruebaTecnicaNet.EventBus.Abstractions;
+using PruebaTecnicaNet.EventBus.Events;
+using PruebaTecnicaNet.Infraestructure;
+using PruebaTecnicaNet.IntegrationEventLogEF.Services;
+
+namespace PruebaTecnicaNet.API.Application.IntegrationEvents;
+
+public class SettlementBankIntegrationEventService(IEventBus eventBus,
+    SettlementBankContext settlementBankContext,
+    IIntegrationEventLogService integrationEventLogService,
+    ILogger<SettlementBankIntegrationEventService> logger) : ISettlementBankIntegrationEventService
+{
+    private readonly IEventBus _eventBus = eventBus ?? throw new ArgumentNullException(nameof(eventBus));
+    private readonly SettlementBankContext _settlementBankContext = settlementBankContext ?? throw new ArgumentNullException(nameof(settlementBankContext));
+    private readonly IIntegrationEventLogService _eventLogService = integrationEventLogService ?? throw new ArgumentNullException(nameof(integrationEventLogService));
+    private readonly ILogger<SettlementBankIntegrationEventService> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+    public async Task PublishEventsThroughEventBusAsync(Guid transactionId)
+    {
+        var pendingLogEvents = await _eventLogService.RetrieveEventLogsPendingToPublishAsync(transactionId);
+
+        foreach (var logEvt in pendingLogEvents)
+        {
+            _logger.LogInformation("Publishing integration event: {IntegrationEventId} - ({@IntegrationEvent})", logEvt.EventId, logEvt.IntegrationEvent);
+
+            try
+            {
+                await _eventLogService.MarkEventAsInProgressAsync(logEvt.EventId);
+                await _eventBus.PublishAsync(logEvt.IntegrationEvent);
+                await _eventLogService.MarkEventAsPublishedAsync(logEvt.EventId);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error publishing integration event: {IntegrationEventId}", logEvt.EventId);
+
+                await _eventLogService.MarkEventAsFailedAsync(logEvt.EventId);
+            }
+        }
+    }
+
+    public async Task AddAndSaveEventAsync(IntegrationEvent evt)
+    {
+        _logger.LogInformation("Enqueuing integration event {IntegrationEventId} to repository ({@IntegrationEvent})", evt.Id, evt);
+
+        await _eventLogService.SaveEventAsync(evt, _settlementBankContext.GetCurrentTransaction());
+    }
+}
