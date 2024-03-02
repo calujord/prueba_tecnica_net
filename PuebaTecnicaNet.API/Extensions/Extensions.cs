159fdd776ce6f043a4023dc315bf51157ec6cbc0
David Lozano Rivada <dlrivada@hotmail.com>
2024-03-02 23:09:15 +0100

    The most significant changes involve the addition of a new static class `Extensions` and the introduction of several namespaces to the `Extensions.cs` file. The `Extensions` class contains two methods, `AddApplicationServices` and `AddEventBusSubscriptions`, which are used to add services to the application and subscriptions to the event bus, respectively.
    The `AddApplicationServices` method includes several important changes such as disabling DbContext pooling for the `SettlementBankContext`, adding migration for the same, and adding various services like `IIntegrationEventLogService`, `ISettlementBankIntegrationEventService`, RabbitMQ event bus, and event bus subscriptions. It also configures MediatR and adds behaviors for logging and validation, registers the validator for the `CreateSettlementBankCommand`, and adds scoped services for `ISettlementBankQueries`, `ISettlementBankRepository`, and `IRequestManager`.
    
    The `AddEventBusSubscriptions` method adds a subscription for the `SettlementBankUpdatedSucceedIntegrationEvent`.
    
    Changes:
    1. Addition of several namespaces to the `Extensions.cs` file.
    2. Addition of a new static class `Extensions`.
    3. Addition of the `AddApplicationServices` method to the `Extensions` class.
    4. Disabling of DbContext pooling for the `SettlementBankContext` in the `AddApplicationServices` method.
    5. Addition of migration for the `SettlementBankContext` in the `AddApplicationServices` method.
    6. Addition of various services in the `AddApplicationServices` method.
    7. Configuration of MediatR and addition of behaviors for logging and validation in the `AddApplicationServices` method.
    8. Registration of the validator for the `CreateSettlementBankCommand` in the `AddApplicationServices` method.
    9. Addition of scoped services in the `AddApplicationServices` method.
    10. Addition of the `AddEventBusSubscriptions` method to the `Extensions` class.
    11. Addition of a subscription for the `SettlementBankUpdatedSucceedIntegrationEvent` in the `AddEventBusSubscriptions` method.

---
+++ PuebaTecnicaNet.API/Extensions/Extensions.cs
@@ -0,0 +1,68 @@
+﻿using PruebaTecnicaNet.Infraestructure;
+using PruebaTecnicaNet.IntegrationEventLogEF.Services;
+using PruebaTecnicaNet.API.Application.IntegrationEvents;
+using PruebaTecnicaNet.API.Infrastructure;
+using PruebaTecnicaNet.API;
+using PruebaTecnicaNet.API.Application.Commands;
+using PruebaTecnicaNet.API.Application.Validations;
+using PruebaTecnicaNet.API.Application.Queries;
+using PruebaTecnicaNet.Domain.SettlementBankAggregate;
+using PruebaTecnicaNet.Infraestructure.Repositories;
+using PruebaTecnicaNet.Infraestructure.Idempotency;
+using PruebaTecnicaNet.API.Application.IntegrationEvents.EventHandling;
+using PruebaTecnicaNet.API.Application.IntegrationEvents.Events;
+using PruebaTecnicaNet.API.Application.Behaviors;
+using PruebaTecnicaNet.Application.Behaviors;
+
+internal static class Extensions
+{
+    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    {
+        var services = builder.Services;
+        
+        // Add the authentication services to DI
+        // builder.AddDefaultAuthentication();
+
+        // Pooling is disabled because of the following error:
+        // Unhandled exception. System.InvalidOperationException:
+        // The DbContext of type 'SettlementBankContext' cannot be pooled because it does not have a public constructor accepting a single parameter of type DbContextOptions or has more than one constructor.
+        builder.AddNpgsqlDbContext<SettlementBankContext>("settlementBankdb", settings => settings.DbContextPooling = false);
+
+        services.AddMigration<SettlementBankContext, SettlementBankContextSeed>();
+
+        // Add the integration services that consume the DbContext
+        services.AddTransient<IIntegrationEventLogService, IntegrationEventLogService<SettlementBankContext>>();
+
+        services.AddTransient<ISettlementBankIntegrationEventService, SettlementBankIntegrationEventService>();
+
+        builder.AddRabbitMqEventBus("eventbus")
+               .AddEventBusSubscriptions();
+
+        services.AddHttpContextAccessor();
+        // services.AddTransient<IIdentityService, IdentityService>();
+
+        // Configure mediatR
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblyContaining(typeof(Program));
+
+            cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            cfg.AddOpenBehavior(typeof(ValidatorBehavior<,>));
+            // cfg.AddOpenBehavior(typeof(TransactionBehavior<,>));
+        });
+
+        // Register the command validators for the validator behavior (validators based on FluentValidation library)
+        services.AddSingleton<IValidator<CreateSettlementBankCommand>, CreateSettlementBankCommandValidator>();
+
+        services.AddScoped<ISettlementBankQueries, SettlementBankQueries>();
+        services.AddScoped<ISettlementBankRepository, SettlementBankRepository>();
+        services.AddScoped<IRequestManager, RequestManager>();
+        // services.AddScoped<SettlementBankServices>();
+
+    }
+
+    private static void AddEventBusSubscriptions(this IEventBusBuilder eventBus)
+    {
+        eventBus.AddSubscription<SettlementBankUpdatedSucceedIntegrationEvent, SettlementBankUpdatedSucceedIntegrationEventHandler>();
+    }
+}
