f259c83b237b9b5080cd794f0d8ab83744694e1d
David Lozano Rivada <dlrivada@hotmail.com>
2024-03-02 23:13:52 +0100

    The most significant changes involve the introduction of new classes and handlers to manage the creation of settlement banks and ensure idempotency in the command process.
    1. `CreateSettlementBankCommand.cs` has been updated to include a new command class `CreateSettlementBankCommand` that implements `IRequest<bool>`. This class is used to create a new settlement bank with properties `Bic`, `Name`, and `Country` which are set through the constructor and are read-only after being set.
    
    2. `CreateSettlementBankCommandHandler.cs` has been updated to include two new command handler classes. The `CreateSettlementBankCommandHandler` class implements `IRequestHandler<CreateSettlementBankCommand, bool>` and contains the logic for handling the `CreateSettlementBankCommand`. The `CreateSettlementBankIdentifiedCommandHandler` class extends `IdentifiedCommandHandler<CreateSettlementBankCommand, bool>` and is used for ensuring idempotency in the command process.
    
    3. `IdentifiedCommand.cs` has been updated to include a new class `IdentifiedCommand<T, R>` which implements `IRequest<R>`. This class is used to wrap a command and an ID together to check for duplicate requests.
    
    4. `IdentifiedCommandHandler.cs` has been updated to include a new abstract class `IdentifiedCommandHandler<T, R>` which implements `IRequestHandler<IdentifiedCommand<T, R>, R>`. This class provides a base implementation for handling duplicate requests and ensuring idempotent updates.

---
+++ PuebaTecnicaNet.API/Application/Commands/CreateSettlementBankCommand.cs
@@ -0,0 +1,33 @@
+﻿namespace PruebaTecnicaNet.API.Application.Commands;
+
+// DDD and CQRS patterns comment: Note that it is recommended to implement immutable Commands
+// In this case, its immutability is achieved by having all the setters as private
+// plus only being able to update the data just once, when creating the object through its constructor.
+// References on Immutable Commands:  
+// http://cqrs.nu/Faq
+// https://docs.spine3.org/motivation/immutability.html 
+// http://blog.gauffin.org/2012/06/griffin-container-introducing-command-support/
+// https://docs.microsoft.com/dotnet/csharp/programming-guide/classes-and-structs/how-to-implement-a-lightweight-class-with-auto-implemented-properties
+
+// using PruebaTecnicaNet.API.Application.Models;
+using PruebaTecnicaNet.API.Extensions;
+
+[DataContract]
+public class CreateSettlementBankCommand : IRequest<bool>
+{
+    [DataMember]
+    public string Bic { get; private set; }
+
+    [DataMember]
+    public string Name { get; private set; }
+
+    [DataMember]
+    public string Country { get; private set; }
+
+    public CreateSettlementBankCommand(string bic, string name, string country)
+    {
+        Bic = bic;
+        Name = name;
+        Country = country;
+    }
+}
---
+++ PuebaTecnicaNet.API/Application/Commands/CreateSettlementBankCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿namespace PruebaTecnicaNet.API.Application.Commands;
+
+using PruebaTecnicaNet.Domain.SettlementBankAggregate;
+using PruebaTecnicaNet.Infraestructure.Idempotency;
+
+// Regular CommandHandler
+public class CreateSettlementBankCommandHandler : IRequestHandler<CreateSettlementBankCommand, bool>
+{
+    private readonly ISettlementBankRepository _settlementBankRepository;
+    private readonly ILogger<CreateSettlementBankCommandHandler> _logger;
+
+    // Using DI to inject infrastructure persistence Repositories
+    public CreateSettlementBankCommandHandler(
+           ISettlementBankRepository settlementBankRepository,
+           ILogger<CreateSettlementBankCommandHandler> logger)
+    {
+        _settlementBankRepository = settlementBankRepository ?? throw new ArgumentNullException(nameof(settlementBankRepository));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+    }
+
+    public async Task<bool> Handle(CreateSettlementBankCommand message, CancellationToken cancellationToken)
+    {
+        // TODO: Add Integration event
+
+        // Add/Update the SettlementBank AggregateRoot
+        // DDD patterns comment: Add child entities and value-objects through the SettlementBank Aggregate-Root
+        // methods and constructor so validations, invariants and business logic 
+        // make sure that consistency is preserved across the whole aggregate
+        var settlementBank = new SettlementBank(message.Bic, message.Country, message.Name);
+
+        _logger.LogInformation("----- Creating SettlementBank - SettlementBank: {@SettlementBank}", settlementBank);
+        if (_settlementBankRepository.GetAsync(settlementBank.Bic) != null)
+            return true;
+
+        _settlementBankRepository.Add(settlementBank);
+
+        return await _settlementBankRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken);
+    }
+}
+
+// Use for Idempotency in Command process
+public class CreateSettlementBankIdentifiedCommandHandler : IdentifiedCommandHandler<CreateSettlementBankCommand, bool>
+{
+    public CreateSettlementBankIdentifiedCommandHandler(
+        IMediator mediator,
+        IRequestManager requestManager,
+        ILogger<IdentifiedCommandHandler<CreateSettlementBankCommand, bool>> logger)
+        : base(mediator, requestManager, logger)
+    {
+    }
+
+    protected override bool CreateResultForDuplicateRequest()
+    {
+        return true;
+    }
+}
+
