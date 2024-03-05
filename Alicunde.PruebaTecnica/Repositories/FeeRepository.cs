2c49f667e521e3bce5faf2e8d10ed3b099ae01f6
Sergio <sergiomartin0x@outlook.com>
2024-03-05 08:52:59 +0100

    crear repositorios

---
+++ Alicunde.PruebaTecnica/Repositories/FeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using Alicunde.PruebaTecnica.Models;
+
+namespace Alicunde.PruebaTecnica.Repositories
+{
+    public class FeeRepository : IFeeRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public FeeRepository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        }
+
+        public async Task<IEnumerable<Fee>> GetAllAsync()
+        {
+            return await _dbContext.Fees.ToListAsync();
+        }
+
+        public async Task AddAsync(Fee fee)
+        {
+            _dbContext.Fees.Add(fee);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Alicunde.PruebaTecnica/Repositories/IFeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using Alicunde.PruebaTecnica.Models;
+
+namespace Alicunde.PruebaTecnica.Repositories
+{
+    public interface IFeeRepository
+    {
+        public interface IFeeRepository
+        {
+            Task<IEnumerable<Fee>> GetAllAsync();
+            Task AddAsync(Fee fee);
+            Task GetByIdAsync(Fee fee);
+        }
+    }
+}
