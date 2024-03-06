d242acb7675f70a6007467d653e434253ef07164
Sergio <sergiomartin0x@outlook.com>
2024-03-06 08:44:13 +0100

    añadido feeRepository

--- Alicunde.PruebaTecnica/Repositories/IFeeRepository.cs
+++ Alicunde.PruebaTecnica/Repositories/IFeeRepository.cs
@@ -1,14 +1,13 @@
-﻿using Alicunde.PruebaTecnica.Models;
+﻿using Alicunde.PruebaTecnica.Data;
+using Alicunde.PruebaTecnica.Models;
 
 namespace Alicunde.PruebaTecnica.Repositories
 {
     public interface IFeeRepository
     {
-        public interface IFeeRepository
-        {
-            Task<IEnumerable<Fee>> GetAllAsync();
-            Task AddAsync(Fee fee);
-            Task GetByIdAsync(Fee fee);
-        }
+        Task<IEnumerable<Fee>> GetAllAsync();
+        Task AddAsync(Fee fee);
+        Task AddRangeAsync(IEnumerable<Fee> fee);
+        Task<Fee?> GetByIdAsync(int id);
     }
 }
